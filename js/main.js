00fc99a36bd77202411fb610cb75ede0d3a8010f
Pasha <askerovpasha21@gmail.com>
2022-11-08 12:13:51 +0400

    add finished products

--- js/main.js
+++ js/main.js
@@ -1,130 +1,21 @@
-
+let pageUpScrollBtn = document.querySelector(".page-up-scroll-btn");
+window.addEventListener("scroll", function(){
+    if(scrollY > 400){
+        pageUpScrollBtn.classList.remove("d-none")
+    }else{
+        pageUpScrollBtn.classList.add("d-none")
+    }
+});
+pageUpScrollBtn.addEventListener("click",function(){
+    window.scrollTo(0,0);
+})
 //MENU BUTTON
 let mobileMenuButton = document.querySelector(".mobile-menu-button");
 mobileMenuButton.addEventListener("click", () => {
     mobileMenuButton.classList.toggle("open")
 });
 
-// STAR BUTTONS
-let productStars = document.querySelectorAll(".product-stars");
-for(let i = 0; i < productStars.length; i++){
-    (function(index){
-        let whiteStars = productStars[index].querySelectorAll(".star-w");
-        let yellowStars = productStars[index].querySelectorAll(".star-y");
-        whiteStars[0].addEventListener("click",function(){
-            whiteStars[0].classList.add("d-none");
-            yellowStars[0].classList.remove("d-none");
-        });
-        yellowStars[0].addEventListener("click",function(){
-            if(yellowStars[1].classList.contains("d-none")==false){
-                yellowStars[1].classList.add("d-none");
-                yellowStars[2].classList.add("d-none");
-                yellowStars[3].classList.add("d-none");
-                yellowStars[4].classList.add("d-none");
-                whiteStars[1].classList.remove("d-none");
-                whiteStars[2].classList.remove("d-none");
-                whiteStars[3].classList.remove("d-none");
-                whiteStars[4].classList.remove("d-none");
-            }else{
-                yellowStars[0].classList.add("d-none");
-                whiteStars[0].classList.remove("d-none");
-            }
-        });
-        whiteStars[1].addEventListener("click",function(){
-            whiteStars[0].classList.add("d-none");
-            whiteStars[1].classList.add("d-none");
-            yellowStars[0].classList.remove("d-none");
-            yellowStars[1].classList.remove("d-none");
-        });
-        yellowStars[1].addEventListener("click",function(){
-            if(yellowStars[2].classList.contains("d-none")==false){
-                yellowStars[2].classList.add("d-none");
-                yellowStars[3].classList.add("d-none");
-                yellowStars[4].classList.add("d-none");
-                whiteStars[2].classList.remove("d-none");
-                whiteStars[3].classList.remove("d-none");
-                whiteStars[4].classList.remove("d-none");
-            }else{
-                yellowStars[0].classList.add("d-none");
-                yellowStars[1].classList.add("d-none");
-                whiteStars[0].classList.remove("d-none");
-                whiteStars[1].classList.remove("d-none");
-            }
-        });
-        whiteStars[2].addEventListener("click",function(){
-            whiteStars[0].classList.add("d-none");
-            whiteStars[1].classList.add("d-none");
-            whiteStars[2].classList.add("d-none");
-            yellowStars[0].classList.remove("d-none");
-            yellowStars[1].classList.remove("d-none");
-            yellowStars[2].classList.remove("d-none");
-        });
-        yellowStars[2].addEventListener("click",function(){
-            if(yellowStars[3].classList.contains("d-none")==false){
-                yellowStars[3].classList.add("d-none");
-                yellowStars[4].classList.add("d-none");
-                whiteStars[3].classList.remove("d-none");
-                whiteStars[4].classList.remove("d-none");
-            }else{
-                yellowStars[0].classList.add("d-none");
-                yellowStars[1].classList.add("d-none");
-                yellowStars[2].classList.add("d-none");
-                whiteStars[0].classList.remove("d-none");
-                whiteStars[1].classList.remove("d-none");
-                whiteStars[2].classList.remove("d-none");
-            }
-        });
-        whiteStars[3].addEventListener("click",function(){
-            whiteStars[0].classList.add("d-none");
-            whiteStars[1].classList.add("d-none");
-            whiteStars[2].classList.add("d-none");
-            whiteStars[3].classList.add("d-none");
-            yellowStars[0].classList.remove("d-none");
-            yellowStars[1].classList.remove("d-none");
-            yellowStars[2].classList.remove("d-none");
-            yellowStars[3].classList.remove("d-none");
-        });
-        yellowStars[3].addEventListener("click",function(){
-            if(yellowStars[4].classList.contains("d-none")==false){
-                yellowStars[4].classList.add("d-none");
-                whiteStars[4].classList.remove("d-none");
-            }else{
-                yellowStars[0].classList.add("d-none");
-                yellowStars[1].classList.add("d-none");
-                yellowStars[2].classList.add("d-none");
-                yellowStars[3].classList.add("d-none");
-                whiteStars[0].classList.remove("d-none");
-                whiteStars[1].classList.remove("d-none");
-                whiteStars[2].classList.remove("d-none");
-                whiteStars[3].classList.remove("d-none");
-            }
-        });
-        whiteStars[4].addEventListener("click",function(){
-            whiteStars[0].classList.add("d-none");
-            whiteStars[1].classList.add("d-none");
-            whiteStars[2].classList.add("d-none");
-            whiteStars[3].classList.add("d-none");
-            whiteStars[4].classList.add("d-none");
-            yellowStars[0].classList.remove("d-none");
-            yellowStars[1].classList.remove("d-none");
-            yellowStars[2].classList.remove("d-none");
-            yellowStars[3].classList.remove("d-none");
-            yellowStars[4].classList.remove("d-none");
-        });
-        yellowStars[4].addEventListener("click",function(){
-            yellowStars[0].classList.add("d-none");
-            yellowStars[1].classList.add("d-none");
-            yellowStars[2].classList.add("d-none");
-            yellowStars[3].classList.add("d-none");
-            yellowStars[4].classList.add("d-none");
-            whiteStars[0].classList.remove("d-none");
-            whiteStars[1].classList.remove("d-none");
-            whiteStars[2].classList.remove("d-none");
-            whiteStars[3].classList.remove("d-none");
-            whiteStars[4].classList.remove("d-none");
-        });   
-    })(i);
-};
+
 
 // SEBET BUTTON
 let sebetButton = document.querySelectorAll(".sebet-button");
